ddb3c7c546ed095eb905b4c7c208e9717acce620
Ju7ii <julian.rok@gmx.de>
2023-12-01 00:17:42 +0100

    Refactor quiz app to reset result array after each game and adjust result text conditions

--- js/app.js
+++ js/app.js
@@ -5,62 +5,47 @@ const QuizSelection = document.getElementById('quiz-selection');
 const LoadingQuestions = document.getElementById('loading-questions');
 const QuestionGame = document.getElementById('question-game');
 const gameResultHTML = document.getElementById('game-result');
-const result = [];
-
+let result = [];
 // Event listener for when the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", function () {
     initializeLogoFadeIn();
 });
-
-// Function to initialize logo fade-in animation
+// Funtion to initialize logo fade-in animation
 function initializeLogoFadeIn() {
     // Show the logo container
     LogoContainer.classList.remove('d-none');
-
     // Logo fade-in animation
     setTimeout(() => {
-        console.log('FADE IN');
         LogoSVG.classList.add('active');
     }, 500);
-
     // Logo fade-out animation
     setTimeout(() => {
-        console.log('FADE OUT');
         LogoSVG.classList.remove('active');
     }, 4000);
-
     // Hide the logo container after fade-out and proceed to quiz selection
     setTimeout(() => {
-        console.log('REMOVE WRAPPER');
         LogoContainer.classList.add('d-none');
         initializeQuizSelectionFadeIn();
     }, 7000);
 }
-
 // Function to initialize quiz selection fade-in animation
 function initializeQuizSelectionFadeIn() {
     QuizSelection.classList.remove('d-none');
 }
-
 // Async function to initialize quiz with a specified category
 async function initializeQuiz(category) {
     // Hide quiz selection and show loading indicator
     QuizSelection.classList.add('d-none');
     LoadingQuestions.classList.remove('d-none');
-
     // Record start time before making a server request
     const startTime = new Date().getTime();
-
     try {
         // Fetch questions from the server
         const questions = await getQuestions(category);
-
         // Record end time after the server request
         const endTime = new Date().getTime();
-
         // Ensure a minimum display time of 500ms, accounting for server request time
         const remainingTime = Math.max(500 - (endTime - startTime), 0);
-
         // Show quiz questions after loading and processing
         setTimeout(() => {
             LoadingQuestions.classList.add('d-none');
@@ -71,19 +56,16 @@ async function initializeQuiz(category) {
         console.error(error);
     }
 }
-
 // Function to make an asynchronous request to get quiz questions
 function getQuestions(category) {
     return new Promise((resolve, reject) => {
         var xhr = new XMLHttpRequest();
         var url = `https://quiz-ofe1.onrender.com/api/${category}`;
-
         // Set up the XMLHttpRequest
         xhr.open('GET', url, true);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
         xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-
         // Handle the XMLHttpRequest state changes
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4) {
@@ -97,12 +79,10 @@ function getQuestions(category) {
                 }
             }
         };
-
         // Send the XMLHttpRequest
         xhr.send();
     });
 }
-
 // Function to start the quiz with provided questions
 function startQuiz(questions) {
     // Selecting quiz-related DOM elements
@@ -113,11 +93,7 @@ function startQuiz(questions) {
     const quizQuestions = questions.data;
     const questionCount = document.getElementById('question-count');
     let questionCounter = 1;
-
     let selectedAnswerIndex;
-
-    console.table(quizQuestions);
-
     // Event listeners for radio button changes
     radioButtons.forEach((radio, index) => {
         radio.addEventListener('change', () => {
@@ -127,47 +103,40 @@ function startQuiz(questions) {
             }
             // Record the selected answer index
             selectedAnswerIndex = index + 1;
-            console.log(selectedAnswerIndex);
         });
     });
-
     // Async function to display and process quiz questions
     async function displayQuestions() {
+        // Set back previous values after first game
+        result = [];
         for (const element of quizQuestions) {
             // Update question count display
             questionCount.textContent = `Question ${questionCounter}/10`;
-
             // Reset radio buttons and disable submit button
             radioButtons.forEach((element) => {
                 element.checked = false;
             });
             submitButton.disabled = true;
-
             // Fill HTML with question and answers
             questionHTML.textContent = element.question;
             const answers = [element.answer1, element.answer2, element.answer3, element.answer4];
             answers.forEach((answer, index) => {
                 answerElementsHTML[index].textContent = answer;
             });
-
             // Wait for user to submit an answer
             await waitForSubmit(submitButton);
             // Check the answer and update the display
             await checkAnswer(selectedAnswerIndex, element.correct_answer, answerElementsHTML, submitButton);
-
             // Increment the question counter
             questionCounter++;
         }
-
         // Hide the question game and show the quiz result
         QuestionGame.classList.add('d-none');
         showResult();
     }
-
     // Start displaying quiz questions
     displayQuestions();
 }
-
 // Async function to wait for the user to submit an answer
 async function waitForSubmit(button) {
     return new Promise((resolve) => {
@@ -178,7 +147,6 @@ async function waitForSubmit(button) {
         });
     });
 }
-
 // Function to check the selected answer against the correct answer
 function checkAnswer(selectedAnswerIndex, correctAnswer, answerElementsHTML, button) {
     return new Promise((resolve) => {
@@ -187,10 +155,6 @@ function checkAnswer(selectedAnswerIndex, correctAnswer, answerElementsHTML, but
         Array.from(inputs).forEach(input => {
             input.disabled = true;
         });
-
-        console.log(selectedAnswerIndex);
-        console.log(correctAnswer);
-
         if (selectedAnswerIndex == correctAnswer) {
             // Display correct answer and update result array
             answerElementsHTML[selectedAnswerIndex - 1].classList.remove('btn-outline-primary');
@@ -204,7 +168,6 @@ function checkAnswer(selectedAnswerIndex, correctAnswer, answerElementsHTML, but
             answerElementsHTML[correctAnswer - 1].classList.add('btn-success');
             result.push(false);
         }
-
         // Update button text and add event listener for next question
         button.textContent = 'Continue questions';
         button.addEventListener('click', () => {
@@ -223,7 +186,6 @@ function checkAnswer(selectedAnswerIndex, correctAnswer, answerElementsHTML, but
         });
     });
 }
-
 // Function to display the quiz result
 function showResult() {
     const resultTextHTML = document.getElementById('result-text');
@@ -231,19 +193,17 @@ function showResult() {
     let resultText;
     // Display result text based on the number of correct answers
     gameResultHTML.classList.remove('d-none');
-
-    if (trueCount == 10) {
+    if (trueCount === 10) {
         resultText = `Excellent, you answered all ${trueCount} questions correctly`;
-    } else if (trueCount <= 9 && trueCount >= 5) {
+    } else if (trueCount >= 6 && trueCount <= 9) {
         resultText = `Very good, you answered ${trueCount} correct questions`;
-    } else if (trueCount <= 5 && trueCount >= 1) {
-        resultText = `You answered ${trueCount} correct questions`
+    } else if (trueCount >= 1 && trueCount < 6) {
+        resultText = `You answered ${trueCount} correct questions`;
     } else {
         resultText = 'You might want to try another quiz';
     }
     resultTextHTML.textContent = resultText;
 }
-
 // Function to reset and start a new quiz
 function newQuiz() {
     gameResultHTML.classList.add('d-none');
